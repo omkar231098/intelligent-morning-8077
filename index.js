8ea6f9be177ff61b205d3df17ddf2be24c3d9b79
Omkar Dhanave <o.m.dhanave@gmail.com>
2022-11-11 12:34:09 +0530

    added day-3 coding

--- index.js
+++ index.js
@@ -28,5 +28,38 @@ function showSlides(n) {
 }
 
 
+// Mid Section Sliding Window
 
+
+
+let slideIndex1 = 1;
+showSlides1(slideIndex1);
+
+// Next/previous controls
+function plusSlides1(n) {
+  showSlides1(slideIndex1 += n);
+}
+
+// Thumbnail image controls
+function currentSlide1(n) {
+  showSlides1(slideIndex1 = n);
+}
+
+function showSlides1(n) {
+  let i;
+  let slides1 = document.getElementsByClassName("mySlides1");
+  let dots1 = document.getElementsByClassName("dot1");
+  if (n > slides1.length) {slideIndex1 = 1}
+  if (n < 1) {slideIndex1 = slides1.length}
+  for (i = 0; i < slides1.length; i++) {
+    slides1[i].style.display = "none";
+  }
+  for (i = 0; i < dots1.length; i++) {
+    dots1[i].className = dots1[i].className.replace(" active1", "");
+  }
+  slides1[slideIndex1-1].style.display = "block";
+  dots1[slideIndex1-1].className += " active1";
+}
+
+// Mid Section 6 iamges Sliding Window
 
